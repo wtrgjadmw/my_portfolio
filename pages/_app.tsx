ad6e27f238c858445ae5cdc824fe399321ac0ea2
fukuda <fukuda.m0113@icloud.com>
2021-03-06 23:09:57 +0900

    app.tsxを型安全に

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import "../styles/globals.css"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      {/* <link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" /> */}
+      {/* <link rel="manifest" href="/manifest.json" /> */}
+    </Head>
+    <Component {...pageProps} />
+  </>
+)
 
-export default MyApp
+export default App
