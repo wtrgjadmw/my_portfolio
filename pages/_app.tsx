96a82dbe062c3fcedeb538f8cbefae92eeee4a77
fukuda <fukuda.m0113@icloud.com>
2021-03-06 23:03:17 +0900

    Revert "tailwindがdeployした後反映されるように"
    This reverts commit 329d269b7ddcf64c2f09337aa1e456748c3641d2.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import "styles/globals.css"
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["{pages}/**/*.{js,jsx,ts,tsx}"],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
