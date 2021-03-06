20816be740d59bab7bb5c6419e33867b26141daa
fukuda <fukuda.m0113@icloud.com>
2021-03-06 16:49:56 +0900

    class→classNameに変更

--- components/svgs/TerminalIcon.tsx
+++ components/svgs/TerminalIcon.tsx
@@ -1,6 +1,6 @@
 export default function TerminalIcon(){
   return(
-  <svg class="h-6 w-6 sm:h-6 sm:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <svg className="h-6 w-6 sm:h-6 sm:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
   </svg>
   )
