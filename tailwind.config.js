65935013469cd945b94673c2c4c5078ae25eccac
fukuda <fukuda.m0113@icloud.com>
2021-03-07 22:21:12 +0900

    tailwindのpurge変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["pages/**/*.{js,jsx,ts,tsx}"],
+  purge: ["{pages,components}/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
